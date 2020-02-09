035c041fdeee3786d922977d2c88bd4c5333fa25
Ming <297236646@qq.com>
2020-02-09 21:52:52 +0800

    init some files

---
+++ app/Http/Controllers/Web/AppController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AppController extends Controller
+{
+
+    public function getApp()
+    {
+        return('app');
+    }
+
+}
